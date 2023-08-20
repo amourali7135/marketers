ee7e27d5f4b2239ba4654af7e1ab2695aa223427
Amir Mourali <amourali7135@gmail.com>
2023-08-21 02:35:04 +0800

    Routes initial trial run done

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,29 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :portfolio, shallow: true do   #skipping join table
+    resources :work#, only: [:create, :new, :index] may not even need it nested
+  end
+
+  resources :proposal, shallow: true do   #skipping join table
+    resources :jobbid#, only: [:create, :new, :index] may not even need it nested
+  end
+
+  resources :question, shallow: true do   #skipping join table
+    resources :answer#, only: [:create, :new, :index] may not even need it nested
+  end
+
+  resources :consultation, shallow: true do   #skipping join table
+    resources :consultationreceipt, only: [:create] 
+    resources :consultationsession, only: [:create, :new]
+  end
+
+  resources :service, except: [:index], shallow: true do   #skipping join table
+    resources :order, shallow: true do #, only: [:create, :new, :index] may not even need it nested
+      resources :review
+      resources :servicereceipt, only: [:create, :show] #maybe remove show
+    end
+  end
+
 end
